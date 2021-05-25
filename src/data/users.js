b02ebc6c3d32ffb0d0fb03da0b8c76e3594781bf
iwebwork <iwebwork.adm@gmail.com>
2021-05-24 21:59:35 -0300

    --- Auto-Git Commit ---

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id:1,
+        name:"User 1",
+        email:"one@gmail.com",
+        avatar:"https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_1280.png"
+    },
+    {
+        id:2,
+        name:"User 2",
+        email:"two@gmail.com",
+        avatar:"https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_1280.png"
+    },
+    {
+        id:3,
+        name:"User 3",
+        email:"tree@gmail.com",
+        avatar:"https://cdn.pixabay.com/photo/2015/11/06/11/43/businessman-1026415_1280.jpg"
+    },
+    {
+        id:4,
+        name:"User 4",
+        email:"four@gmail.com",
+        avatar:"https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_1280.png"
+    },
+]
